0327dc6e8679022afe84d25a79fae8701d27066a
nicole <nicole732470@gmail.com>
2026-05-21 18:23:26 -0500

    Add high_priority column to todos for priority marking

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "defaults high_priority to false" do
+    todo = Todo.new(description: "Example")
+
+    assert_not todo.high_priority
+  end
+
+  test "can save with high_priority true" do
+    todo = Todo.create!(description: "Important", high_priority: true)
+
+    assert todo.high_priority
+  end
 end
